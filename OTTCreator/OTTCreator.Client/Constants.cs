f2482de6dc205bfb3a2d101b6c9ee630f180e7ef
Borys Kopytko <kopytkoborys@gmail.com>
2023-05-21 21:57:42 +0300

    Create Constants.cs

---
+++ OTTCreator/OTTCreator.Client/Constants.cs
@@ -0,0 +1,12 @@
+﻿public static class Constants
+{
+    public const string DatabaseFileName = "OTTCreatorClientDatabase.db3";
+
+    public const SQLite.SQLiteOpenFlags Flags =
+        SQLite.SQLiteOpenFlags.ReadWrite |
+        SQLite.SQLiteOpenFlags.Create |
+        SQLite.SQLiteOpenFlags.SharedCache;
+
+    public static string DatabasePath =>
+        Path.Combine(FileSystem.AppDataDirectory, DatabaseFileName);
+}
