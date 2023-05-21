5ea00ebffbd2c50ebaf40d042a10f21e2862e016
Borys Kopytko <kopytkoborys@gmail.com>
2023-05-21 22:39:27 +0300

    Create ClientDatabase.cs

---
+++ OTTCreator/OTTCreator.Client/Data/ClientDatabase.cs
@@ -0,0 +1,27 @@
+﻿using OTTCreator.Client.Models;
+using SQLite;
+
+namespace OTTCreator.Client.Data
+{
+    public class ClientDatabase
+    {
+        SQLiteAsyncConnection clientDatabase;
+
+        public ClientDatabase() { }
+
+        async Task Init()
+        {
+            if (clientDatabase is not null)
+                return;
+
+            clientDatabase = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.flags);
+            var result = await clientDatabase.CreateTableAsync<ContentItem>();
+        }
+
+        public async Task<List<ContentItem>> GetItemsAsync()
+        {
+            await Init();
+            return await clientDatabase.Table<ContentItem>().ToListAsync();
+        }
+    }
+}
