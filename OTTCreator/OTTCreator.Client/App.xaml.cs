d918aed71ba51924408e32954842c093d1e5e93e
Borys Kopytko <kopytkoborys@gmail.com>
2023-05-24 23:29:17 +0300

    Update audio support for ContentItemMediaElement and update support for SQLite for Android

--- OTTCreator/OTTCreator.Client/App.xaml.cs
+++ OTTCreator/OTTCreator.Client/App.xaml.cs
@@ -6,15 +6,13 @@ namespace OTTCreator.Client
 {
     public partial class App : Application
     {
-        bool isTestDataNeeded;
         ClientDatabase clientDatabase;
 
         public App()
         {
             InitializeComponent();
 
-            isTestDataNeeded = false;
-            if (isTestDataNeeded)
+            if (Constants.isTestDataNeeded)
             {
                 clientDatabase = new ClientDatabase();
                 var task = Task.Run(GetTestData);
@@ -30,21 +28,21 @@ namespace OTTCreator.Client
 
         private async Task GetTestData()
         {
-            var contentItemForCreate = new ContentItem() { Name = "Test content 1", Category = "Test category A", Type = "Test type I", Image = new System.Uri("https://cdn.pixabay.com/photo/2023/04/11/22/08/flower-7918323_960_720.jpg"), Stream = new System.Uri("https://ythls.onrender.com/channel/UCkyrSWEcjZKpIwMxiPfOcgg.m3u8") };
+            var contentItemForCreate = new ContentItem() { Name = "Test content 1", Category = "Test category A", Type = "Test type I", HasVideo = true, Image = new System.Uri("https://cdn.pixabay.com/photo/2023/04/11/22/08/flower-7918323_960_720.jpg"), Stream = new System.Uri("https://ythls.onrender.com/channel/UCkyrSWEcjZKpIwMxiPfOcgg.m3u8") };
             await clientDatabase.SaveItemAsync(contentItemForCreate);
-            contentItemForCreate = new ContentItem() { Name = "Test content 2", Category = "Test category A", Type = "Test type I", Image = new System.Uri("https://cdn.pixabay.com/photo/2023/04/11/22/08/flower-7918323_960_720.jpg"), Stream = new System.Uri("http://hbbtvlive.v3.tvp.pl/hbbtvlive/livestream.php?app_id=tvpinfo") };
+            contentItemForCreate = new ContentItem() { Name = "Test content 2", Category = "Test category A", Type = "Test type I", HasVideo = true, Image = new System.Uri("https://cdn.pixabay.com/photo/2023/04/11/22/08/flower-7918323_960_720.jpg"), Stream = new System.Uri("http://hbbtvlive.v3.tvp.pl/hbbtvlive/livestream.php?app_id=tvpinfo") };
             await clientDatabase.SaveItemAsync(contentItemForCreate);
-            contentItemForCreate = new ContentItem() { Name = "Test content 3", Category = "Test category B", Type = "Test type I", Image = new System.Uri("https://cdn.pixabay.com/photo/2023/04/11/22/08/flower-7918323_960_720.jpg"), Stream = new System.Uri("https://ythls.onrender.com/channel/UCkyrSWEcjZKpIwMxiPfOcgg.m3u8") };
+            contentItemForCreate = new ContentItem() { Name = "Test content 3", Category = "Test category B", Type = "Test type I", HasVideo = true, Image = new System.Uri("https://cdn.pixabay.com/photo/2023/04/11/22/08/flower-7918323_960_720.jpg"), Stream = new System.Uri("https://ythls.onrender.com/channel/UCkyrSWEcjZKpIwMxiPfOcgg.m3u8") };
             await clientDatabase.SaveItemAsync(contentItemForCreate);
-            contentItemForCreate = new ContentItem() { Name = "Test content 4", Category = "Test category B", Type = "Test type I", Image = new System.Uri("https://cdn.pixabay.com/photo/2023/04/11/22/08/flower-7918323_960_720.jpg"), Stream = new System.Uri("http://hbbtvlive.v3.tvp.pl/hbbtvlive/livestream.php?app_id=tvpinfo") };
+            contentItemForCreate = new ContentItem() { Name = "Test content 4", Category = "Test category B", Type = "Test type I", HasVideo = true, Image = new System.Uri("https://cdn.pixabay.com/photo/2023/04/11/22/08/flower-7918323_960_720.jpg"), Stream = new System.Uri("http://hbbtvlive.v3.tvp.pl/hbbtvlive/livestream.php?app_id=tvpinfo") };
             await clientDatabase.SaveItemAsync(contentItemForCreate);
-            contentItemForCreate = new ContentItem() { Name = "Test content 5", Category = "Test category C", Type = "Test type II", Image = new System.Uri("https://cdn.pixabay.com/photo/2023/04/11/22/08/flower-7918323_960_720.jpg"), Stream = new System.Uri("http://radio.ukr.radio:8000/ur1-lv-mp3") };
+            contentItemForCreate = new ContentItem() { Name = "Test content 5", Category = "Test category C", Type = "Test type II", HasVideo = false, Image = new System.Uri("https://cdn.pixabay.com/photo/2023/04/11/22/08/flower-7918323_960_720.jpg"), Stream = new System.Uri("http://radio.ukr.radio:8000/ur1-lv-mp3") };
             await clientDatabase.SaveItemAsync(contentItemForCreate);
-            contentItemForCreate = new ContentItem() { Name = "Test content 6", Category = "Test category C", Type = "Test type II", Image = new System.Uri("https://cdn.pixabay.com/photo/2023/04/11/22/08/flower-7918323_960_720.jpg"), Stream = new System.Uri("http://icecast.luxnet.ua/luxlviv") };
+            contentItemForCreate = new ContentItem() { Name = "Test content 6", Category = "Test category C", Type = "Test type II", HasVideo = false, Image = new System.Uri("https://cdn.pixabay.com/photo/2023/04/11/22/08/flower-7918323_960_720.jpg"), Stream = new System.Uri("http://icecast.luxnet.ua/luxlviv") };
             await clientDatabase.SaveItemAsync(contentItemForCreate);
-            contentItemForCreate = new ContentItem() { Name = "Test content 7", Category = "Test category D", Type = "Test type II", Image = new System.Uri("https://cdn.pixabay.com/photo/2023/04/11/22/08/flower-7918323_960_720.jpg"), Stream = new System.Uri("http://radio.ukr.radio:8000/ur1-lv-mp3") };
+            contentItemForCreate = new ContentItem() { Name = "Test content 7", Category = "Test category D", Type = "Test type II", HasVideo = false, Image = new System.Uri("https://cdn.pixabay.com/photo/2023/04/11/22/08/flower-7918323_960_720.jpg"), Stream = new System.Uri("http://radio.ukr.radio:8000/ur1-lv-mp3") };
             await clientDatabase.SaveItemAsync(contentItemForCreate);
-            contentItemForCreate = new ContentItem() { Name = "Test content 8", Category = "Test category D", Type = "Test type II", Image = new System.Uri("https://cdn.pixabay.com/photo/2023/04/11/22/08/flower-7918323_960_720.jpg"), Stream = new System.Uri("http://icecast.luxnet.ua/luxlviv") };
+            contentItemForCreate = new ContentItem() { Name = "Test content 8", Category = "Test category D", Type = "Test type II", HasVideo = false, Image = new System.Uri("https://cdn.pixabay.com/photo/2023/04/11/22/08/flower-7918323_960_720.jpg"), Stream = new System.Uri("http://icecast.luxnet.ua/luxlviv") };
             await clientDatabase.SaveItemAsync(contentItemForCreate);
         }
     }
--- OTTCreator/OTTCreator.Client/Pages/ContentItemPage.xaml.cs
+++ OTTCreator/OTTCreator.Client/Pages/ContentItemPage.xaml.cs
@@ -16,12 +16,22 @@ namespace OTTCreator.Client
         protected override async void OnAppearing()
         {
             var currentID = await SecureStorage.Default.GetAsync("CurrentID");
+            if (currentID == null)
+                currentID = "1";
             var currentItem = await clientDatabase.GetItemAsync(Convert.ToInt32(currentID));
             var currentStream = currentItem.Stream.ToString();
             if (currentStream != ContentItemMediaElement.Source.ToString().Replace("Uri: ", ""))
             {
                 ContentItemMediaElement.Source = currentStream;
-                Image.Source = currentItem.Image;
+                if (!currentItem.HasVideo)
+                {
+                    AudioCoverImage.Source = currentItem.Image;
+                    AudioCoverImage.IsVisible = true;
+                }
+                else
+                {
+                    AudioCoverImage.IsVisible = false;
+                }
                 Title = currentItem.Name;
             }
             
